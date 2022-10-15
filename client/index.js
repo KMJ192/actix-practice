8c007543229ab8d5916a9298004bc94d4b4ef216
MyeongJunKim <myeongjun222@naver.com>
2022-10-15 20:47:46 +0900

    cache 설정

--- client/index.js
+++ client/index.js
@@ -1,13 +1,3 @@
 import wasm from './pkg/client.js';
 
 (await wasm()).app();
-
-const socket = new WebSocket(`ws://${window.location.host}/ws/`);
-
-// setInterval(() => {
-//   socket.send('test2');
-// }, 2000);
-
-socket.addEventListener('message', (msg) => {
-  console.log(msg.data);
-});
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 use std::process::Command;
 
-use actix_web::{web, get, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, http::header::{CacheControl, CacheDirective}};
 use actix_files::Files;
-use actix_web_actors::ws;
-// use notify::{Watcher, RecommendedWatcher, RecursiveMode, Result};
 
 pub mod render;
-pub mod wss;
-pub mod file_watcher;
 use render::{RenderParam, render};
-use wss::Wss;
-use file_watcher::file_watcher;
-
 
 #[get("/")]
 async fn root_page() -> impl Responder {
-  HttpResponse::Ok().body(render(RenderParam {
-    ssr: String::from("<div>root</div>"),
-    meta: String::from(""),
-  }))
-}
-
-async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-  ws::start(Wss {}, &req, stream)
+  HttpResponse::Ok()
+    .insert_header(CacheControl(
+      vec![
+        CacheDirective::NoCache,
+        CacheDirective::NoStore,
+        CacheDirective::MustRevalidate,
+        // CacheDirective::MaxAge(0)
+      ]
+    ))
+    .body(render(RenderParam {
+      ssr: String::from("<div>root</div>"),
+      meta: String::from(""),
+    }))
 }
 
 #[actix_web::main]
@@ -39,13 +37,10 @@ async fn main() -> std::io::Result<()> {
       .expect("failed to execute process in linux");
   };
 
-  let f = file_watcher();
-
   let application = HttpServer::new(|| {
     App::new()
       .service(root_page)
       .service(Files::new("/src", "./client").index_file("index.js"))
-      .route("/ws/", web::get().to(index))
   })
   .bind(("127.0.0.1", 8080))?
   .run()
